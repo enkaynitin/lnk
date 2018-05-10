5205fdb66d0749edd3a8f698ba50567d29b4cce6
Nitin kumar <enkaynitin@github.com>
2018-05-10 16:29:06 +0530

    Adding mysql db

--- lnkblog/lnkblog/settings.py
+++ lnkblog/lnkblog/settings.py
@@ -86,13 +86,21 @@ WSGI_APPLICATION = 'lnkblog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/etc/mysql/my.cnf',
+        },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
